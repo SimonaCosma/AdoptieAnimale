b1639e48b35cdead61ec1c9590ef98e0e9b6d831
SIMONA-ANDREA COSMA <simona.cosma@stud.ubbcluj.ro>
2026-01-10 20:21:17 +0200

    Reorganize: Monorepo cu Mobile, Web, WebAPI; adaugă .gitignore

---
+++ AdoptieAnimale.Mobile/AdoptieAnimale.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AdoptieAnimale.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AdoptieAnimale.Mobile/AdoptieAnimale.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdoptieAnimale.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
